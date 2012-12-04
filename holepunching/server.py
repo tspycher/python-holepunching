a486afd591ac4f7d5c3f869aaafd670b0f5fc951
Thomas Spycher <me@tspycher.com>
2012-12-04 19:31:27 +0100

    enhanced server to send connectioninformation to both parties

--- holepunching/server.py
+++ holepunching/server.py
@@ -4,6 +4,8 @@ Created on Dec 4, 2012
 @author: thospy
 '''
 from SocketServer import UDPServer, BaseRequestHandler #,ThreadingMixIn,ThreadingUDPServer,ForkingUDPServer
+import socket
+
 import base64
 import os
 import threading
@@ -26,27 +28,40 @@ class Server(BaseRequestHandler):
         
     def handle(self):        
         print "message: %s from %s " %  (self.request[0], self.client_address)
-        socket = self.request[1]
-        backval = self.addRemotes(self.client_address)
-        if backval:
-            socket.sendto(backval, self.client_address)
+        #socket = self.request[1]
+        self.addRemotes(self.request[0], self.client_address)
+        #if backval:
+            #socket.sendto(backval, self.client_address)
     
     def addRemotes(self, identifier, address):
         if not identifier in self._remotesPool:
             self._remotesPool[identifier] = []
             
+        #self._remotesPool[identifier].append([address[0], address[1], socket])
         self._remotesPool[identifier].append(address)
-        
+
+        #self._remotesPool[identifier].append(socket)
+
         print self._remotesPool
         
-        if len(self._remotesPool) < 2:
+        if len(self._remotesPool[identifier]) < 2:
             print "having still less than 2 connections"
             return None
         
         print "having two Clients"
         #self._remotesPool = []
         #print self._remotesPool
-        return ":%s,%i" % (self._remotesPool[identifier][0][0], self._remotesPool[identifier][0][1])
+        connectionStringA = ":%s,%i" % (self._remotesPool[identifier][0][0], self._remotesPool[identifier][0][1])
+        connectionStringB = ":%s,%i" % (self._remotesPool[identifier][1][0], self._remotesPool[identifier][1][1])
+        #socketA = self._remotesPool[identifier][0][2]
+        #socketB = self._remotesPool[identifier][1][2]
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+        sock.sendto(connectionStringB,self._remotesPool[identifier][0])
+        sock.sendto(connectionStringA,self._remotesPool[identifier][1])
+
+        self._remotesPool.pop(identifier)
+        #return connectionString
         #print self._remotesPool
             
     @staticmethod
