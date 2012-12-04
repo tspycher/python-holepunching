64f7dc74a49c7403fa5e726082ec2497a2c29303
Thomas Spycher <me@tspycher.com>
2012-12-04 17:47:58 +0100

    Added initial Base Structure

---
+++ holepunching/server.py
@@ -0,0 +1,7 @@
+'''
+Created on Dec 4, 2012
+
+@author: thospy
+'''
+class Server(object):
+    pass
---
+++ runit.py
@@ -0,0 +1,6 @@
+from holepunching import Server, Client
+
+
+
+if __name__ == "__main__":
+    print "Running"
